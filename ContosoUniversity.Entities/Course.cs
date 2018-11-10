d41c97087e142cfc68353fe795b0fa0bb9bce70c
Felipe Romero <feliperomero3@outlook.com>
2018-11-10 12:59:39 -0700

    Add Course, Enrollment and Student entities.

---
+++ ContosoUniversity.Entities/Course.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace ContosoUniversity.Entities
+{
+    public class Course
+    {
+        public int CourseId { get; set; }
+
+        public string Code { get; set; }
+
+        public string Title { get; set; }
+
+        public int Credits { get; set; }
+
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
---
+++ ContosoUniversity.Entities/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ContosoUniversity.Entities
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime EnrollmentDate { get; set; }
+
+
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
