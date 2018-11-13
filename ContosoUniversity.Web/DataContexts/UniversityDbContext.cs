a6ad0e6378578c1a7e1feba329d148725ff7788d
Felipe Romero <feliperomero3@outlook.com>
2018-11-12 18:03:00 -0700

    Add UniversityDbContext.

---
+++ ContosoUniversity.Web/DataContexts/UniversityDbContext.cs
@@ -0,0 +1,16 @@
+﻿using ContosoUniversity.Entities;
+using System.Data.Entity;
+
+namespace ContosoUniversity.Web.DataContexts
+{
+    public class UniversityDbContext
+    {
+        public UniversityDbContext() { }
+
+        public DbSet<Student> Students { get; set; }
+
+        public DbSet<Course> Courses { get; set; }
+
+        public DbSet<Enrollment> Enrollments { get; set; }
+    }
+}
