f4d0b6ee4a97c801d2f3242efda67e5a9cb4c4dd
Felipe Romero <feliperomero3@outlook.com>
2018-11-13 08:07:29 -0700

    Override OnModelCreating on IdentityDb.

--- ContosoUniversity.Web/DataContexts/IdentityDb.cs
+++ ContosoUniversity.Web/DataContexts/IdentityDb.cs
@@ -1,4 +1,6 @@
-﻿using ContosoUniversity.Web.Models;
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using ContosoUniversity.Web.Models;
 using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace ContosoUniversity.Web.DataContexts
@@ -10,6 +12,21 @@ namespace ContosoUniversity.Web.DataContexts
         public static IdentityDb Create()
         {
             return new IdentityDb();
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.HasDefaultSchema("Identity");
+
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            modelBuilder.Entity<ApplicationUser>().ToTable("AspNetUser");
+            modelBuilder.Entity<IdentityRole>().ToTable("AspNetRole");
+            modelBuilder.Entity<IdentityUserLogin>().ToTable("AspNetUserLogin");
+            modelBuilder.Entity<IdentityUserRole>().ToTable("AspNetUserRole");
+            modelBuilder.Entity<IdentityUserClaim>().ToTable("AspNetUserClaim");
         }
     }
 }
