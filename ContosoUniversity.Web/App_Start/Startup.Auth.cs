aface767f0ed07d460ec6d0c4ed6f3c5d6838cb0
Felipe Romero <feliperomero3@outlook.com>
2018-11-12 18:00:29 -0700

    Rename ApplicationDbContext to IdentityDb.

--- ContosoUniversity.Web/App_Start/Startup.Auth.cs
+++ ContosoUniversity.Web/App_Start/Startup.Auth.cs
@@ -16,7 +16,7 @@ namespace ContosoUniversity.Web
         public void ConfigureAuth(IAppBuilder app)
         {
             // Configure the db context, user manager and signin manager to use a single instance per request
-            app.CreatePerOwinContext(ApplicationDbContext.Create);
+            app.CreatePerOwinContext(IdentityDb.Create);
             app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
             app.CreatePerOwinContext<ApplicationSignInManager>(ApplicationSignInManager.Create);
 
---
+++ ContosoUniversity.Web/DataContexts/IdentityDb.cs
@@ -0,0 +1,15 @@
+﻿using ContosoUniversity.Web.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace ContosoUniversity.Web.DataContexts
+{
+    public class IdentityDb : IdentityDbContext<ApplicationUser>
+    {
+        public IdentityDb() { }
+
+        public static IdentityDb Create()
+        {
+            return new IdentityDb();
+        }
+    }
+}
