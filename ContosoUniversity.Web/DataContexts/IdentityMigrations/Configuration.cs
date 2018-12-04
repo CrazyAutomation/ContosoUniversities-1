5ae46a49bbe26a57ea88693a117d1f1dd0e45cd2
Felipe Romero <feliperomero3@outlook.com>
2018-12-04 10:07:40 -0700

    Remove redundant qualifiers in IdentityDb configuration.

--- ContosoUniversity.Web/DataContexts/IdentityMigrations/Configuration.cs
+++ ContosoUniversity.Web/DataContexts/IdentityMigrations/Configuration.cs
@@ -1,11 +1,8 @@
+using System.Data.Entity.Migrations;
+
 namespace ContosoUniversity.Web.DataContexts.IdentityMigrations
 {
-    using System;
-    using System.Data.Entity;
-    using System.Data.Entity.Migrations;
-    using System.Linq;
-
-    internal sealed class Configuration : DbMigrationsConfiguration<ContosoUniversity.Web.DataContexts.IdentityDb>
+    internal sealed class Configuration : DbMigrationsConfiguration<IdentityDb>
     {
         public Configuration()
         {
@@ -13,7 +10,7 @@ namespace ContosoUniversity.Web.DataContexts.IdentityMigrations
             MigrationsDirectory = @"DataContexts\IdentityMigrations";
         }
 
-        protected override void Seed(ContosoUniversity.Web.DataContexts.IdentityDb context)
+        protected override void Seed(IdentityDb context)
         {
             //  This method will be called after migrating to the latest version.
 
