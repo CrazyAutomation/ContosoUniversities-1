8e688eb2a9c7512661e736a57f6f5034e71beb65
Felipe Romero <feliperomero3@outlook.com>
2019-03-02 10:36:39 -0700

    Remove unused using directives.

--- ContosoUniversity.Web/Controllers/AccountController.cs
+++ ContosoUniversity.Web/Controllers/AccountController.cs
@@ -1,16 +1,13 @@
-﻿using System;
-using System.Globalization;
-using System.Linq;
-using System.Security.Claims;
+﻿using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
-using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.Owin;
-using Microsoft.Owin.Security;
 using ContosoUniversity.Web.Identity;
 using ContosoUniversity.Web.Models;
 using ContosoUniversity.Web.ViewModels;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
 
 namespace ContosoUniversity.Web.Controllers
 {
--- ContosoUniversity.Web/Controllers/ManageController.cs
+++ ContosoUniversity.Web/Controllers/ManageController.cs
@@ -1,13 +1,12 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using ContosoUniversity.Web.Identity;
+using ContosoUniversity.Web.ViewModels;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
-using ContosoUniversity.Web.Identity;
-using ContosoUniversity.Web.ViewModels;
 
 namespace ContosoUniversity.Web.Controllers
 {
